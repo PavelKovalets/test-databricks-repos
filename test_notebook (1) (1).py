2b77c763fa265fe03af26cbcb5b069a30ccdbed2
PavelKovalets <PavelKovalets>
2022-10-11 23:33:05 +0000

    commit

---
+++ test_notebook (1) (1).py
@@ -0,0 +1,32 @@
+# Databricks notebook source
+# MAGIC %md
+# MAGIC # Some title
+
+# COMMAND ----------
+
+print("hi there!")
+
+# COMMAND ----------
+
+x = 2
+
+# COMMAND ----------
+
+print(f"{x}nd version")
+
+# COMMAND ----------
+
+# MAGIC %sh ls .
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC #Andohter title
+
+# COMMAND ----------
+
+print("more!")
+
+# COMMAND ----------
+
+
---
+++ test_notebook (1).py
@@ -0,0 +1,36 @@
+# Databricks notebook source
+# MAGIC %md
+# MAGIC # Some title
+
+# COMMAND ----------
+
+print("hello!")
+
+# COMMAND ----------
+
+print("hi there!")
+
+# COMMAND ----------
+
+x = 2
+
+# COMMAND ----------
+
+print(f"{x}nd version")
+
+# COMMAND ----------
+
+# MAGIC %sh ls .
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC #Andohter title
+
+# COMMAND ----------
+
+print("more!")
+
+# COMMAND ----------
+
+
